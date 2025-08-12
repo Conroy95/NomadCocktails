4caecd4daa930ed72a41a1c7d458838d90042a68
Conroy95 <leandervdneut@ziggo.nl>
2025-08-12 21:30:50 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,35 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "cocktail-cache-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/cocktails.json",
+  "/manifest.json",
+  "/icons/cocktail-192.png",
+  "/icons/cocktail-512.png"
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('cocktail-cache').then(cache => {
-      return cache.addAll(['/', '/index.html', '/css/style.css', '/js/app.js', '/cocktails.json']);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
