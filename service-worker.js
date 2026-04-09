e92575bc4f8b8aea0a721718f78ce7492780e3c6
Conroy95 <leandervdneut@ziggo.nl>
2026-04-09 20:00:05 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,26 @@
-const CACHE_NAME = "cocktail-cache-v1";
+const CACHE_NAME = "nomad-cocktails-v2";
 const ASSETS = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/app.js",
-  "/cocktails.json",
-  "/manifest.json",
-  "/icons/cocktail-192.png",
-  "/icons/cocktail-512.png"
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/app.js",
+  "./cocktails.json",
+  "./manifest.json",
+  "./icons/cocktail-192.png",
+  "./icons/cocktail-512.png"
 ];
 
+// Installeren van de Service Worker en cachen van assets
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS);
     })
   );
+  self.skipWaiting();
 });
 
+// Verwijderen van oude caches bij een update
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
@@ -26,10 +29,17 @@ self.addEventListener("activate", event => {
   );
 });
 
+// Slimme fetch-strategie: Cache eerst, dan netwerk update
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(cachedResponse => {
+      const fetchPromise = fetch(event.request).then(networkResponse => {
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+        });
+        return networkResponse;
+      });
+      return cachedResponse || fetchPromise;
     })
   );
 });
